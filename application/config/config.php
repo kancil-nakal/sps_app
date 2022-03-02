c1c933a18af968526b65a100c873927a57eb9cd5
kancil-nakal <kancilnakal@protonmail.com>
2022-03-02 10:25:33 +0700

    merubah config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://gms.tpm-security.com/';
-$config['base_url'] = 'http://localhost:8080/gms/';
+$config['base_url'] = 'https://gms.tpm-security.com/';
+// $config['base_url'] = 'http://localhost:8080/gms/';
 
 /*
 |--------------------------------------------------------------------------
